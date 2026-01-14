3d9ec80f63113b2fb691107817b2b8db7f5ad731
SklTlzz <sould5485@gmail.com>
2026-01-14 16:58:25 +0300

    исправлены ошибки и отредактирован base.html и product_detail.html

--- enf/cart/models.py
+++ enf/cart/models.py
@@ -77,4 +77,4 @@ class CartItem(models.Model):
 
     @property
     def total_price(self):
-        return Decimal(str(self.product.price) * self.quantity)
+        return Decimal(str(self.product.price)) * self.quantity
